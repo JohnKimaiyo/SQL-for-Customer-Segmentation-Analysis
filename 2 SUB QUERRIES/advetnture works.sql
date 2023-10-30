8a76c652d57c0c2ba67fdf643ac8005b90e2b41a
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-10-30 16:51:58 +0300

    final commir

--- 2 SUB QUERRIES/advetnture works.sql
+++ 2 SUB QUERRIES/advetnture works.sql
@@ -2,3 +2,13 @@
 SELECT  [JobTitle]
   FROM [AdventureWorks2019].[HumanResources].[Employee]
   WHERE VacationHours =(SELECT Max(VacationHours) from [AdventureWorks2019].[HumanResources].[Employee] );
+
+  -- select date with highest exchae rate ---
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT  [BusinessEntityID]
+      ,[RateChangeDate]
+      ,[Rate]
+      ,[PayFrequency]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2019].[HumanResources].[EmployeePayHistory]
+  WHERE [Rate]  =  (SELECT Max(Rate)   FROM [AdventureWorks2019].[HumanResources].[EmployeePayHistory])
