e42ace75404c22aea873f0bea320bd38de22b67e
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-10-30 18:50:43 +0300

    final comit

--- 2 SUB QUERRIES/world db.sql
+++ 2 SUB QUERRIES/world db.sql
@@ -1,3 +1,7 @@
 -- select city with hightest poplulation --
 SELECT * FROM world.city
 WHERE Population = (SELECT MAX(Population)  FROM world.city );
+
+-- select region with the hihets ;ife expectancy
+SELECT * FROM world.country
+WHERE LifeExpectancy=(SELECT MAX(LifeExpectancy) FROM world.country     )
