f81c3a7ff1bef8e05f5960e868ba86b3b589a2ff
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-10-31 09:31:31 +0300

    final comit

--- 2 SUB QUERRIES/world db.sql
+++ 2 SUB QUERRIES/world db.sql
@@ -5,3 +5,9 @@ WHERE Population = (SELECT MAX(Population)  FROM world.city );
 -- select region with the hihets ;ife expectancy
 SELECT * FROM world.country
 WHERE LifeExpectancy=(SELECT MAX(LifeExpectancy) FROM world.country     )
+
+-- select language with the highest percentage --
+SELECT * FROM world.countrylanguage
+WHERE Percentage=(SELECT MAX(Percentage)
+FROM world.countrylanguage
+)
