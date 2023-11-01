f797a3d704e18ee76d8397d35ce047e5b0a77f65
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-01 16:26:54 +0300

    final commit

--- 2 SUB QUERRIES/bank.sql
+++ 2 SUB QUERRIES/bank.sql
@@ -2,3 +2,6 @@
 SELECT * FROM bank.account
 WHERE balance =(select Max(balance) FROM bank.account  );
 
+-- select type of loan with high interest rate --
+SELECT * FROM bank.loan_type
+WHERE base_interest_rate = (SELECT MAX(base_interest_rate)  FROM bank.loan_type  )
