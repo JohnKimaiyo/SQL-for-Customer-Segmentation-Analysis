57160363c1591ec5e8732d791f30537807612bbd
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-01 15:40:37 +0300

    final commit

--- 2 SUB QUERRIES/northwinds.sql
+++ 2 SUB QUERRIES/northwinds.sql
@@ -4,3 +4,12 @@ SELECT TOP (1000) [OrderID]
     [Quantity]
   FROM [Northwinds Database].[dbo].[Order Details]
   WHERE Quantity=(select max(Quantity)    FROM [Northwinds Database].[dbo].[Order Details] )
+
+  -- select product name with highest reorder level --- 
+  SELECT TOP (1000) [ProductID]
+      ,[ProductName]
+      
+      ,[ReorderLevel]
+     
+  FROM [Northwinds Database].[dbo].[Products]
+  WHERE ReorderLevel=(SELECT MAX(ReorderLevel)FROM [Northwinds Database].[dbo].[Products]   )
