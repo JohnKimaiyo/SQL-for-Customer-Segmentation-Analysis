9fed6f61e5de097e4a984a049e4d861c337dfcc1
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-05 16:20:59 +0300

    final commit

---
+++ 2 SUB QUERRIES/freecodecamp.sql
@@ -0,0 +1,9 @@
+-- select  salary more than the avaerage --
+SELECT TOP (1000) [id]
+      ,[name]
+      ,[role]
+      ,[wage]
+      ,[country]
+  FROM [freecodecamp dataset].[dbo].[Sheet1$]
+
+  WHERE wage < (SELECT   AVG(wage) FROM [freecodecamp dataset].[dbo].[Sheet1$] )
