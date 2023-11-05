33b2a0c51624ca4a9f4d32fee4a97850b612b09b
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-05 16:31:45 +0300

    final commit

--- 2 SUB QUERRIES/freecodecamp.sql
+++ 2 SUB QUERRIES/freecodecamp.sql
@@ -7,3 +7,15 @@ SELECT TOP (1000) [id]
   FROM [freecodecamp dataset].[dbo].[Sheet1$]
 
   WHERE wage > (SELECT   AVG(wage) FROM [freecodecamp dataset].[dbo].[Sheet1$] )
+
+  -- salary greater than averaget salart in usa --
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [id]
+      ,[name]
+      ,[role]
+      ,[wage]
+      ,[country]
+  FROM [freecodecamp dataset].[dbo].[Sheet1$]
+  WHERE country IN (SELECT country
+    FROM [freecodecamp dataset].[dbo].[Sheet1$]
+  WHERE country = 'USA');
