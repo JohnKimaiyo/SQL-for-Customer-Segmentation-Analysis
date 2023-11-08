dbd4ee9b5d786eb34684769534b17fc05531fc30
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-08 18:24:25 +0300

    final commit

---
+++ 11 SQL ClAUSES/UPDATE/adventureworks.sql
@@ -0,0 +1,22 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+update [HumanResources].Employee
+set VacationHours = VacationHours + 10;
+
+
+SELECT TOP (1000) [BusinessEntityID]
+      ,[NationalIDNumber]
+      ,[LoginID]
+      ,[OrganizationNode]
+      ,[OrganizationLevel]
+      ,[JobTitle]
+      ,[BirthDate]
+      ,[MaritalStatus] 
+      ,[Gender]
+      ,[HireDate]
+      ,[SalariedFlag]
+      ,[VacationHours]
+      ,[SickLeaveHours]
+      ,[CurrentFlag]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2019].[HumanResources].[Employee]
