ad5cffdc1ec4edeb42a337a694878469d428b516
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-08 18:44:40 +0300

    final commit

--- 11 SQL ClAUSES/UPDATE/adventureworks.sql
+++ 11 SQL ClAUSES/UPDATE/adventureworks.sql
@@ -20,3 +20,16 @@ SELECT TOP (1000) [BusinessEntityID]
       ,[rowguid]
       ,[ModifiedDate]
   FROM [AdventureWorks2019].[HumanResources].[Employee]
+
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+UPDATE [HumanResources].EmployeePayHistory
+
+SET Rate = Rate +10
+
+SELECT TOP (1000) [BusinessEntityID]
+      ,[RateChangeDate]
+      ,[Rate]
+      ,[PayFrequency]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2019].[HumanResources].[EmployeePayHistory]
