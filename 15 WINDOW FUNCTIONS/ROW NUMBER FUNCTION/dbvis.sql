a6ba95ba45f016d52ba8159c1724e314ce5b8e84
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-15 13:57:17 +0300

    final commit

--- 15 WINDOW FUNCTIONS/ROW NUMBER FUNCTION/dbvis.sql
+++ 15 WINDOW FUNCTIONS/ROW NUMBER FUNCTION/dbvis.sql
@@ -49,3 +49,39 @@ ORDER BY
 DESC
 ) as rank
   FROM [dbvis database].[dbo].[exam_scores]
+
+
+  CREATE TABLE
+ feb_exam_scores (
+    id INT 
+PRIMARY KEY
+,
+    name 
+VARCHAR
+(50),
+    score INT
+);
+INSERT INTO
+ feb_exam_scores (id, name, score)
+VALUES
+    (1, 
+'Alice'
+, 285),
+    (2, 
+'Bob'
+, 292),
+    (3, 
+'Charlie'
+, 278),
+    (4, 
+'Dave'
+,291),
+    (5, 
+'Eve'
+, 289),
+    (6, 
+'John'
+, 292),
+    (7, 
+'Andrew'
+, 285);
