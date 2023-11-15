3b0303ff4784925ecbb13c3e12395a1cf725dfb3
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-15 15:47:46 +0300

    final commit

--- 15 WINDOW FUNCTIONS/RANKING FUNCTIONS/dbdavis.sql
+++ 15 WINDOW FUNCTIONS/RANKING FUNCTIONS/dbdavis.sql
@@ -11,3 +11,11 @@ SELECT TOP (1000) [id]
       ,[score]
 	  ,RANK() OVER(Order By score DESC) AS rank
   FROM [dbvis database].[dbo].[feb_exam_scores]
+
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [id]
+      ,[name]
+      ,[score]
+	  ,RANK() OVER(Order By score DESC) as rank
+  FROM [dbvis database].[dbo].[mar_exam_scores]
