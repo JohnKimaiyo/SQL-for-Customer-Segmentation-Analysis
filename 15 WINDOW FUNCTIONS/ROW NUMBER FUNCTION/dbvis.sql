2e0f8e4fd441d1bca53c26b469890efc37483533
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-15 13:44:08 +0300

    final commit

--- 15 WINDOW FUNCTIONS/ROW NUMBER FUNCTION/dbvis.sql
+++ 15 WINDOW FUNCTIONS/ROW NUMBER FUNCTION/dbvis.sql
@@ -1,3 +1,42 @@
+CREATE TABLE
+ exam_scores (
+    id INT 
+PRIMARY KEY
+,
+    name 
+VARCHAR
+(50),
+    score INT
+);
+INSERT INTO
+ exam_scores (id, name, score)
+VALUES
+    (1, 
+'Alice'
+, 85),
+    (2, 
+'Bob'
+, 92),
+    (3, 
+'Charlie'
+, 78),
+    (4, 
+'Dave'
+, 91),
+    (5, 
+'Eve'
+, 89),
+    (6, 
+'John'
+, 92),
+    (7, 
+'Andrew'
+, 85);
+
+
+
+
+
 
 /****** ROW_NUMBER() function to assign a rank to each row based on the score.  ******/
 SELECT TOP (1000) [id]
