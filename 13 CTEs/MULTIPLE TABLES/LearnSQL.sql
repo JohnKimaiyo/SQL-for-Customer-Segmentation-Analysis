7a5a97105ffa424625be3f44df7fcf9d5a3e6c57
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-16 14:44:24 +0300

    final commit

--- 13 CTEs/MULTIPLE TABLES/LearnSQL.sql
+++ 13 CTEs/MULTIPLE TABLES/LearnSQL.sql
@@ -6,3 +6,12 @@ SELECT TOP (1000) [employee_id]
       ,[bonus]
       ,[average_bonus_for_position]
   FROM [LearnSQL dbo].[dbo].[Bonus_jan]
+
+  WITH avg_position AS (
+    SELECT position, AVG(bonus) AS average_bonus_for_position
+    FROM Bonus_jan
+    GROUP BY position)
+SELECT b.employee_id, b.first_name, b.last_name, b.position, b.bonus, ap.average_bonus_for_position
+FROM Bonus_jan b
+JOIN avg_position ap
+ON b.position = ap.position;
