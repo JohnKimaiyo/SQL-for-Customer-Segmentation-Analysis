e9db02a118e3fd704f8b277a842b5a06bbc650a5
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-11-26 22:56:03 +0300

    final commit

--- 6 FILTERING DATA/filter.sql
+++ 6 FILTERING DATA/filter.sql
@@ -5,3 +5,23 @@ SELECT TOP (1000) [DepartmentID]
       ,[ModifiedDate]
   FROM [AdventureWorks2019].[HumanResources].[Department]
   WHERE year([ModifiedDate]) = '2008';
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [BusinessEntityID]
+      ,[NationalIDNumber]
+      ,[LoginID]
+      ,[OrganizationNode]
+      ,[OrganizationLevel]
+      ,[JobTitle]
+      ,[BirthDate]
+      ,[MaritalStatus]
+      ,[Gender]
+      ,[HireDate]
+      ,[SalariedFlag]
+      ,[VacationHours]
+      ,[SickLeaveHours]
+      ,[CurrentFlag]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2019].[HumanResources].[Employee]
+  WHERE year([BirthDate]) = '1969';
